399e3ee499ab97f85ecb74ec0636c8f11e2fb074
udaykiran1729 <udaykiran.gntr@gmail.com>
2021-12-08 22:20:21 +0530

    commit

--- python project pactice/tkin.py
+++ python project pactice/tkin.py
@@ -1,6 +1,18 @@
 #from tkinter import *
 import tkinter
+from tkinter import *
+from typing import Text
 window=tkinter.Tk()
+window.geometry("450x300")
 window.title("uday")
-label=tkinter.Label(window,text="hello uday welcome to tkinter").pack()
+label=tkinter.Label(window,text="hello uday welcome to tkinter",font="bold").pack()
+txt=tkinter.Entry(window,width=0)
+txt.grid(column=1,row=0)
+def click():
+    res="welcome to"+txt.get()
+    l1.configure(text=res)
+t=tkinter.Button(window,text="enter",command=clicked)
+##f#or i in range(1,10):
+ #   bt=tkinter.Button(window,text=i)
+ #   bt.grid(column=i,row=0)
 window.mainloop()

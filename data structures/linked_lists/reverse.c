f06e3fcc4fce7dbb106a72962758843a16fc4848
udaykiran1729 <udaykiran.gntr@gmail.com>
2021-12-20 22:32:04 +0530

    commit

--- data structures/linked_lists/reverse.c
+++ data structures/linked_lists/reverse.c
@@ -7,7 +7,8 @@ struct node{
 
 struct node* head=NULL;
 struct node* tail=NULL;
-
+struct node* head1=NULL;
+struct node* tail1=NULL;
 void create()
 {
     int i=0;
@@ -19,7 +20,7 @@ void create()
     newnode->data=d;
     newnode->next=newnode;
     if (head==NULL)
-    {
+    {   
         head=newnode;
         tail=newnode;
     }
@@ -43,9 +44,69 @@ void disp(){
     printf(" %d",temp->data);
 }
 
+void disp_1(){
+    temp=head1;
+    //printf("%d",temp->data);
+    printf("\nreverse:\n");
+    while(temp->next!=NULL)
+    {
+        printf(" %d \n",temp->data);
+        temp=temp->next;
+    }
+    printf(" %d",temp->data);
+}
+
+void reverse()
+{
+    
+    /*temp=tail;
+    while(temp->next!=tail)
+    {
+        if (head1==NULL)
+        {
+            head1=temp;
+            tail1=temp;
+        }
+        else
+        {
+            tail1->next=temp;
+            tail1=temp;
+            tail1->next=NULL;
+        }
+        printf("%d")
+    }*/
+    //printf("%d",temp->data);
+
+    //temp=tail;
+    while(temp->next!=head)
+    {
+        temp=tail;
+        if (head1==NULL)
+        {
+            head1=temp;
+            tail1=temp;
+        }
+        else{
+            tail1->next=temp;
+            tail1=temp;
+            temp=head;
+            while (temp->next!=tail)
+            {
+                temp=temp->next;
+            }
+            temp->next=NULL;
+            tail=temp;
+
+        }
+
+    }
+}
+
 void main()
 {
     create();
     //printf("123\n");
     disp();
+    reverse();
+    disp_1();
 }

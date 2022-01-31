893b33f9669ca2c9db956ba01472dff43b5b9d88
udaykiran1729 <udaykiran.gntr@gmail.com>
2022-01-31 21:02:48 +0530

    commit

--- NPTEL/c plus/week_1.cpp
+++ NPTEL/c plus/week_1.cpp
@@ -1,12 +1,17 @@
 #include<iostream>
 #include<string>
+#include<vector>
+
 using namespace std;
-main()
-{
-    string m="Hello World";
-    //m.resize(5);
-    //m.shrink_to_fit();
-    m.copy("Hello",5,0);
-    cout<<m;
+
+/*bool compare(int i,int j){
+    return (i<j);
+}*/
+// declaration of arrays using vector library
+int main(void){
+    string s1="hello";
+    string s2="world";
+    string s=s1+" "+s2;
+    cout<<s;
     return 0;
 }

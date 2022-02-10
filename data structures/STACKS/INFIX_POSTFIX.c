f87eed52ddc23b1ceea3f321bc28a9631cc285f5
udaykiran1729 <udaykiran.gntr@gmail.com>
2022-02-10 22:21:34 +0530

    commit

--- data structures/STACKS/INFIX_POSTFIX.c
+++ data structures/STACKS/INFIX_POSTFIX.c
@@ -1,4 +1,4 @@
-#include<stdio.h>
+/*#include<stdio.h>
 #include<string.h>
 int a[10];
 
@@ -85,7 +85,7 @@ int main(){
         pop();
     }
     /*printf("ELEMENTS AFTER POPING:/n");
-    disp();*/
+    disp();
     char e[10];
     printf("enter the infix expression:");
     gets(e);
@@ -108,14 +108,133 @@ int main(){
                 printf("%c",low);
             }
         }
-        /*else if(e[i]=='('){
+        else if(e[i]=='('){
             push('(');
         }
         else if(e[i]==')'){
             for (char i=a[top];i!='(';i++){
                 printf("%c")
             }
-        }*/
+        }
     }
     return 0;
+}*/
+
+#include<stdio.h>
+#include<string.h>
+
+char a[20];
+
+int top=-1;
+
+void push(char item){
+        if (top>20){
+                printf("STAK OVERFLOW");
+        }
+        else{
+                //top=top+1;
+                a[++top]=item;
+        }
+}
+
+char pop(){
+
+        if (top<0){
+                //printf("STACK UNDERFLOW");
+                return -1;
+        }
+        else{
+                //top=top-1;
+                //printf("%d",a[top]);
+                return a[top--];
+        }
+}
+
+int isopran(char c){
+        if (c>=97 && c<=123){
+                return 1;
+        }
+        else{
+                return 0;
+        }
+}
+
+int isopere(char c){
+        if (c=='*'||c=='/'||c=='+'||c=='-'||c=='^'){
+                return 1;
+        }
+        else{
+                return 0;
+        }
+}
+
+int preced(char c){
+        if (c=='*'||c=='^'||c=='/'){
+                return 2;
+        }
+        else if(c=='+'||c=='-'){
+                return 1;
+        }
+        else{
+                return 0;
+        }
+}
+
+int main(){
+        printf("enter the infix expression:");
+        char ch[20];
+        scanf("%s",ch);
+        //ch='('+ch+')'
+        int len=strlen(ch);
+        //printf("%d",len);
+        //push('(');
+        for (int i=0;i<len;i++){
+                if (isopran(ch[i])){
+                        printf("%c",ch[i]);
+                }
+                else if (ch[i]=='('){
+                        push(ch[i]);
+                }
+                else if(isopere(ch[i])){
+                        if (top==0){
+                                //printf("in the else-if-2 if cond");
+                                push(ch[i]);
+                        }
+                        else{
+                                //printf("in the else-if-2 else cond");
+                                //char temp=pop();
+                                int m1=preced(ch[i]);
+                                int m2=preced(a[top-1]);
+                                if (m1>m2){
+                                        //printf("in the else-if-2 else(ifcond)\n");
+                                        //push(temp);
+                                        push(ch[i]);
+                                }
+                                else{
+                                        //printf("m1<m2\n");
+                                        printf("%c",pop());
+                                        push(ch[i]);
+                                }
+                        }
+
+                }
+                else if(ch[i]==')'){
+                        char x;
+                        while(x=pop()!='('){
+                                printf("%c",x);
+                        }
+                        x=pop();
+                }
+        }
+        //printf("\n\n\n");
+        if(top>=0){
+        while(top>=0){
+                printf("%c",a[top]);
+                top--;
+        }
+        }
+
+
+
+        return 0;
 }

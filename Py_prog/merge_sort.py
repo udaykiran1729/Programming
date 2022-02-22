aad00b9bed0d99c34d658894b64471f365162d97
udaykiran1729 <udaykiran.gntr@gmail.com>
2022-02-22 19:20:15 +0530

    commit

---
+++ Py_prog/merge_sort.py
@@ -0,0 +1,26 @@
+c=[]
+
+m=int(input('enter the length of first list:'))
+n=int(input('enter the length of second list:'))
+
+l1=[int(input('enter the element-a:')) for x in range(m)]
+l1.sort()
+
+l2=[int(input('enter the element-b:')) for x in range(n)]
+l2.sort()
+
+t1,t2=0,0
+while t1+t2<m+n:
+    if t1==m:
+        c.append(l2[t2])
+        t2+=1
+    elif t2==n:
+        c.append(l1[t1])
+        t1+=1
+    elif l1[t1]>l2[t2]:
+        c.append(l2[t2])
+        t2+=1
+    elif l1[t1]<l2[t2]:
+        c.append(l1[t1])
+        t1+=1
+print(c)
--- Py_prog/selection_sort.py
+++ Py_prog/selection_sort.py
@@ -3,12 +3,15 @@
 
 n=int(input('enter the length:'))
 #l=list(range(500,1,-1))
-l=[int(input()) for x in range(n)]
+l=[int(input("enter the list:")) for x in range(n)]
 c=0
 for i in range(len(l)):
+    min=i
     for j in range(i,len(l)):
-        if l[j]>l[i]:
-            l[i],l[j]=l[j],l[i]
-        c+=1
+        if l[min]>l[j]:
+            min=j
+    c+=1
+    l[i],l[min]=l[min],l[i]
+        #c+=1
 print(l)
 print('no of interations:',c)
